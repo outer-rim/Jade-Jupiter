ec96a9e437e76fc2c68198408d0c433ab7a9eaeb
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-03-07 08:30:14 +0530

    Add api call for show operator list

--- src/pages/operators.js
+++ src/pages/operators.js
@@ -1,22 +1,29 @@
 import { OperatorTable, PatientTable, RoomTable } from "../components/Widgets";
-import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
+import {
+  Col,
+  Row,
+  Button,
+  Dropdown,
+  ButtonGroup,
+} from "@themesberg/react-bootstrap";
 import React from "react";
 import { useState, useEffect } from "react";
 import { BACKEND_URL } from "../constants.js";
-import axios from "axios";
 import AugmentedAxios from "../utils/augmentedAxios";
 
-export default() => {
-    const [operators, setOperators] = useState([]);
-    useEffect(() => {
-      AugmentedAxios.get(`${BACKEND_URL}/operator/list`).then((res) => {
+export default () => {
+  const [operators, setOperators] = useState([]);
+  useEffect(() => {
+    AugmentedAxios.get(`${BACKEND_URL}/operator/list`)
+      .then((res) => {
         setOperators(res.data.operators);
-      }).catch((e) => console.log(e));
-    }, []);
-    return (
-        <>
-        <h1 className="text-center">List of All Operators</h1>
-        <OperatorTable operatorList={operators}/>
-        </>
-    );
+      })
+      .catch((e) => console.log(e));
+  }, []);
+  return (
+    <>
+      <h1 className="text-center">List of All Operators</h1>
+      <OperatorTable operatorList={operators} />
+    </>
+  );
 };
