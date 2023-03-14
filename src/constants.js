1d6389f28399fc56f6207deea6f0f981486ba9d4
Kronos-192081 <shiladityade.bwn2001@gmail.com>
2023-03-14 21:59:17 +0530

    Last Push

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-const BACKEND_URL = "http://10.145.152.46:8080/v1";
-const FRONTEND_URL = "http://10.145.152.46:3000";
-const FILE_URL = "http://10.145.152.46:8080"
+const BACKEND_URL = "http://localhost:8080/v1"; // update as required
+const FRONTEND_URL = "http://localhost:3000"; // update as required
+const FILE_URL = "http://localhost:8080"; // update as required
 
 // const BACKEND_URL = "http://localhost:8080/v1";
 // const FRONTEND_URL = "http://localhost:3000";
